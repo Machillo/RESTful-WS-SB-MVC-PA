6a94db9a208c32fed92bc262a4d4ee9c2c2f944a
Kenneth Alvarado <gatotico99@gmail.com>
2023-07-14 12:11:33 -0600

    Added usercontoller, implement, entity, security

---
+++ mobile-app-ws/src/main/java/com/project/app/ws/UserRepository.java
@@ -0,0 +1,12 @@
+package com.project.app.ws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.app.ws.io.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    UserEntity findByEmail(String email);
+}
---
+++ mobile-app-ws/src/main/java/com/project/app/ws/service/UserService.java
@@ -0,0 +1,8 @@
+package com.project.app.ws.service;
+
+import com.project.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+
+    UserDto createUser(UserDto user);
+}
