c68a76e0f57f2a5078f93da4927f8553b5dc2eb2
Kenneth Alvarado <gatotico99@gmail.com>
2023-07-14 12:15:16 -0600

    Update package

--- mobile-app-ws/src/main/java/com/project/app/ws/io/repository/UserRepository.java
+++ mobile-app-ws/src/main/java/com/project/app/ws/io/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.project.app.ws;
+package com.project.app.ws.io.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
