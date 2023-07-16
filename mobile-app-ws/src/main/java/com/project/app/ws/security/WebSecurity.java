4bab4c3be53b4bce9566da638e657e7761db64d0
Kenneth Alvarado <gatotico99@gmail.com>
2023-07-16 14:16:35 -0600

    WebSecurity update

--- mobile-app-ws/src/main/java/com/project/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/project/app/ws/security/WebSecurity.java
@@ -1,45 +1,45 @@
 package com.project.app.ws.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+import com.project.app.ws.service.UserService;
 
-    private final UserDetailsService userDetailsService;
+@EnableWebSecurity
+public class WebSecurity {
+
+    private final UserService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public WebSecurity(UserService userDetailsService,
+            BCryptPasswordEncoder bCryptPasswordEncoder) {
+
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf()
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+
+        http.csrf()
                 .disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
-
-        return http.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
