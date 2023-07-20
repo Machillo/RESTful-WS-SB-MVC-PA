041308675a1d1ae68991b645cfa600b2508885b0
Kenneth Alvarado <gatotico99@gmail.com>
2023-07-20 07:33:23 -0600

    Added Get User

--- mobile-app-ws/src/main/java/com/project/app/ws/io/repository/UserRepository.java
+++ mobile-app-ws/src/main/java/com/project/app/ws/io/repository/UserRepository.java
@@ -9,4 +9,7 @@ import com.project.app.ws.io.entity.UserEntity;
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     UserEntity findByEmail(String email);
+
+    UserEntity findByUserId(String userId);
+
 }
--- mobile-app-ws/src/main/java/com/project/app/ws/service/UserService.java
+++ mobile-app-ws/src/main/java/com/project/app/ws/service/UserService.java
@@ -1,5 +1,6 @@
 package com.project.app.ws.service;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.project.app.ws.shared.dto.UserDto;
@@ -9,4 +10,6 @@ public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto user);
 
     UserDto getUser(String email);
+
+    UserDto getUserByUserId(String userId);
 }
